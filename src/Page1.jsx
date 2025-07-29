389128468028cf205b727aec16524948737d6576
Datta-Prasad <2400031486@kluniversity.in>
2025-07-29 15:17:50 +0530

    version1

---
+++ src/Page1.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Page1 extends Component {
+  render() {
+    return (
+      <div style ={{backgroundColor:"limegreen",height:'100vh'}}>
+        <h1>
+            India is my country
+        </h1>
+      </div>
+    )
+  }
+}
